0f97294923e422f5cd02bd2d6c3dae1caed68d98
prince patel <patelprince3698@gmail.com>
2026-07-06 02:22:47 +0530

    modified:   DevConnect/client/src/socket.js

--- DevConnect/client/src/socket.js
+++ DevConnect/client/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(
-    "http://localhost:5000",
-    
-);
+const socket = io("https://devconnect-1-rbax.onrender.com", {
+  transports: ["websocket"],
+  withCredentials: true,
+});
 
 export default socket;
