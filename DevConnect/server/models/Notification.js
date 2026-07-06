10477cfb5efce6fdc9d7dae7ad9b2735656ee73e
prince patel <patelprince3698@gmail.com>
2026-07-06 12:11:52 +0530

    modified:   DevConnect/server/models/Notification.js

--- DevConnect/server/models/Notification.js
+++ DevConnect/server/models/Notification.js
@@ -1,49 +1,21 @@
+import mongoose from "mongoose";
 
-const notificationSchema = new mongoose.Schema(
-{
-    recipient: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-
+const notificationSchema = new mongoose.Schema({
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     type: {
         type: String,
         enum: ["like", "comment", "follow", "message", "reply", "reaction"],
         required: true,
     },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
+    reel: { type: mongoose.Schema.Types.ObjectId, ref: "Reel" },
+    message: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    entityType: { type: String, enum: ["post", "reel", "message"] },
+    read: { type: Boolean, default: false },
+}, { timestamps: true });
 
-    entityType: {
-        type: String,
-        enum: ["post", "reel", "message"],
-    },
+const Notification = mongoose.model("Notification", notificationSchema);
 
-    post: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post",
-    },
-
-    reel: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Reel",
-    },
-
-    message: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Message",
-    },
-
-    read: {
-        type: Boolean,
-        default: false,
-    },
-},
-{
-    timestamps: true,
-});
+// ✅ DEFAULT EXPORT (IMPORTANT)
+export default Notification;
