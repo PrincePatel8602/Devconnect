b9f79e949d8ad2e532a0ccf5c3cdf0c5363f6e13
prince patel <patelprince3698@gmail.com>
2026-07-06 10:28:28 +0530

    modified:   DevConnect/server/server.js

--- DevConnect/server/server.js
+++ DevConnect/server/server.js
@@ -1,15 +1,26 @@
+import http from "http";
+
+import app from "./app.js";
+import connectDB from "./config/db.js";
+import { initSocket } from "./config/socket.js";
+
 const startServer = async () => {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const server = http.createServer(app);
+        const server = http.createServer(app);
 
-    initSocket(server);
+        initSocket(server);
 
-    const PORT = process.env.PORT || 5000;
+        const PORT = process.env.PORT || 5000;
 
-    server.listen(PORT, () => {
-        console.log(`Server running on ${PORT}`);
-    });
+        server.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
 };
 
 startServer();
